e8fec054594e92dae02cfb35800c08b150d33011
David Morton <davidlmorton@gmail.com>
2013-03-17 12:11:58 -0500

    Fix README and add example

--- examples/hello.py
+++ examples/hello.py
@@ -1,6 +1,3 @@
-Callbacks lets you use decorator syntax to set callbacks on methods or functions.
-
-```python
 from callbacks import supports_callbacks
 
 def callback():
@@ -14,6 +11,3 @@ target.add_callback(callback)
 
 print "This should print 'hello Polly!':"
 target() # prints "hello Polly!"
-```
-
-[![Build Status](https://secure.travis-ci.org/davidlmorton/callbacks.png?branch=master)](https://travis-ci.org/davidlmorton/callbacks)
