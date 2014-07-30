2301cc59ed1040529b1231dd357794d52bb65a24
David Morton <davidlmorton@gmail.com>
2014-07-30 21:54:22 +0000

    Update setup.py to use pbr and start using tox with coverage

--- setup.py
+++ setup.py
@@ -1,25 +1,6 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-        name = 'callbacks',
-        author = 'David Morton',
-        author_email = 'davidlmorton@gmail.com',
-        version = '0.1.3',
-        license = 'LICENSE.txt',
-        description = 'Simple callbacks using decorators',
-        long_description = open('README.txt').read(),
-        url = 'https://github.com/davidlmorton/callbacks',
-        packages = find_packages(exclude=[
-            'tests',
-        ]),
-        install_requires = [
-        ],
-        setup_requires = [
-            'nose',
-        ],
-        tests_require = [
-            'nose',
-            'coverage',
-        ],
-        test_suite = 'tests',
-)
+
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
