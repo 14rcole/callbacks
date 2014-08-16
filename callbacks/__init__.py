124cfc9cf905d4f58c4336e03c223445e784a12e
David Morton <davidlmorton@gmail.com>
2014-08-15 20:07:57 -0500

    Bump version to 0.2.0

--- callbacks/__init__.py
+++ callbacks/__init__.py
@@ -1,5 +1,5 @@
 from .callbacks import supports_callbacks
-__version__ = '0.1.4'
+__version__ = '0.2.0'
 
 __doc__ = """
     This library allows you to place decorators on functions and methods that
